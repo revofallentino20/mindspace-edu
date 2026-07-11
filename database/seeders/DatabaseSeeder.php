9c228b35cd6e386303e8dd23af788a35708311b0
Revo Fallentino <revofallentino@gmail.com>
2026-07-11 18:57:49 +0700

    fix: add sitecontentseeder to databaseseeder to call list

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
             SchoolSeeder::class,
             ScreeningSeeder::class,
             AppointmentSeeder::class,
+            SiteContentSeeder::class,
         ]);
     }
 }
